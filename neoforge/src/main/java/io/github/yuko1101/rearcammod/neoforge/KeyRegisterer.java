4e4a87599d9fd03eff1938e242f44cbcd94dfe05
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2026-03-17 02:21:34 +0900

    removed needless bus option

--- neoforge/src/main/java/io/github/yuko1101/rearcammod/neoforge/KeyRegisterer.java
+++ neoforge/src/main/java/io/github/yuko1101/rearcammod/neoforge/KeyRegisterer.java
@@ -5,7 +5,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
-@EventBusSubscriber(modid = RearCamMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = RearCamMod.MOD_ID)
 public class KeyRegisterer {
     @SubscribeEvent
     public static void onKeyRegister(RegisterKeyMappingsEvent event) {
